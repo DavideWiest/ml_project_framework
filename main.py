dd22f39f5bfacfeb33382daa62c105b59789ea98
Davide Wiest <davide.wiest2@gmail.com>
2022-11-26 16:59:25 +0100

    make basic version

--- main.py
+++ main.py
@@ -2,8 +2,28 @@
 Main execution file
 """
 from logger import logging
-from ml_modules import test_step, train_step, train_full_fn
+from pathlib import Path
+import os, json
+import torch
+from torch import nn
 
+from ml_modules import test_step, train_step, train_full_fn, accuracy_fn_regression
+
+from model import BasicModel
+
+BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR_NAME = os.path.dirname(Path(__file__).resolve())
+CONFIG_JSON_PATH = BASE_DIR / "config.json"
+
+with open(CONFIG_JSON_PATH, "r", encoding="utf-8") as f:
+    config = json.load(f)
+    mconfig = config["model"]
+
+model = BasicModel(mconfig["input_features"], mconfig["output_features"], mconfig["hidden_units"])
+
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=mconfig["lr"])
 
 
 
---
+++ model.py
@@ -0,0 +1,16 @@
+from torch import nn
+
+class BasicModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units):
+        super().__init__()
+
+        self.layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_features)
+        )
+
+    def forward(self, x):
+        return self.layer_stack(x)
