73fe23a2de6fbf686d5b646bacd2fac9a89b8b36
Davide Wiest <davide.wiest2@gmail.com>
2022-11-26 19:30:06 +0100

    add more parameters

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ BASE_DIR = Path(__file__).resolve().parent
 BASE_DIR_NAME = os.path.dirname(Path(__file__).resolve())
 CONFIG_JSON_PATH = BASE_DIR / "config.json"
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+NUM_WORKERS = os.cpu_count()
+
 with open(CONFIG_JSON_PATH, "r", encoding="utf-8") as f:
     config = json.load(f)
     mconfig = config["model"]
 
-model = BasicModel(mconfig["input_features"], mconfig["output_features"], mconfig["hidden_units"])
+model = BasicModel(mconfig["input_features"], mconfig["output_features"], mconfig["hidden_units"]).to(device)
 
 loss_fn = nn.CrossEntropyLoss()
 
-optimizer = torch.optim.SGD(model.parameters(), lr=mconfig["lr"])
+optimizer = torch.optim.SGD(model.parameters(), lr=mconfig["lr"]).to(device)
 
-
+if __name__ == '__main__':
+    ...
 
